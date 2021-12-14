c89a80003255947968a4bcd6f61b6689d6e30783
Greg Wood <contact@gregwood.io>
2021-12-14 10:55:25 -0500

    Day 14 part 2

--- Day14/day14/bin/day14.dart
+++ Day14/day14/bin/day14.dart
@@ -8,7 +8,15 @@ void main(List<String> arguments) {
 }
 
 int solve(List<String> input, [bool isPartTwo = false]) {
-  var polymer = input[0];
+  Map<String, int> pairs = {};
+  for (var i = 0; i < input[0].length - 1; i++) {
+    var pair = input[0].substring(i, i + 2);
+    if (pairs.containsKey(pair)) {
+      pairs[pair] = pairs[pair]! + 1;
+    } else {
+      pairs[pair] = 1;
+    }
+  }
 
   Map<String, String> inst = {};
   for (var i = 2; i < input.length; i++) {
@@ -19,31 +27,49 @@ int solve(List<String> input, [bool isPartTwo = false]) {
   var steps = isPartTwo ? 40 : 10;
 
   for (var step = 0; step < steps; step++) {
-    var newPolymer = '';
+    Map<String, int> newPairs = {};
+    for (var pair in pairs.entries) {
+      var elem = inst[pair.key];
+      var key1 = "${pair.key[0]}$elem";
+      var key2 = "$elem${pair.key[1]}";
 
-    for (var i = 0; i < polymer.length - 1; i++) {
-      var pair = polymer.substring(i, i + 2);
-      newPolymer += '${pair[0]}${inst[pair]}';
+      if (newPairs.containsKey(key1)) {
+        newPairs[key1] = newPairs[key1]! + pair.value;
+      } else {
+        newPairs[key1] = pair.value;
+      }
+      if (newPairs.containsKey(key2)) {
+        newPairs[key2] = newPairs[key2]! + pair.value;
+      } else {
+        newPairs[key2] = pair.value;
+      }
     }
-    newPolymer += polymer[polymer.length - 1];
 
-    polymer = newPolymer;
+    pairs = newPairs;
   }
 
   Map<String, int> counts = {};
-  var letters = polymer.split('');
-  for (var letter in letters) {
-    if (counts.containsKey(letter)) {
-      counts[letter] = counts[letter]! + 1;
-    } else {
-      counts[letter] = 1;
+  for (var pair in pairs.entries) {
+    for (var letter in pair.key.split('')) {
+      if (counts.containsKey(letter)) {
+        counts[letter] = counts[letter]! + pair.value;
+      } else {
+        counts[letter] = pair.value;
+      }
     }
   }
+
+  // all counts are doubled because the pairs overlap.
+  // To get an accurate count we can add one to the first
+  // and last element (which haven't changed) then divide by 2.
+  counts[input[0][0]] = counts[input[0][0]]! + 1;
+  counts[input[0][input[0].length - 1]] =
+      counts[input[0][input[0].length - 1]]! + 1;
 
   var mostCommonElement =
       counts.values.reduce((curr, prev) => curr > prev ? curr : prev);
   var leastCommonElement =
       counts.values.reduce((curr, prev) => curr < prev ? curr : prev);
 
-  return mostCommonElement - leastCommonElement;
+  return (mostCommonElement ~/ 2) - (leastCommonElement ~/ 2);
 }
